d023ca6aa7b784a2828b1e8f2c61cada30e89400
xXAI-botXx <tobia2761@gmail.com>
2024-08-12 17:14:53 +0800

    2D Game update (refactoring + namespace seperating)

--- games/computer_game_1/Game.cpp
+++ games/computer_game_1/Game.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+// link to the interface of this file
 #include "Game.hpp"
 
-void print_field(std::uint32_t player_position){
+// variables only available in this file -> use namespace
+namespace{
+    constexpr std::uint32_t LEN_X = 10U;
+    constexpr std::uint32_t START = 0U;
+    constexpr std::uint32_t GOAL = 9U;
+    constexpr char LEFT = 'a';
+    constexpr char RIGHT = 'd';
+};
+
+void print_field(const std::uint32_t player_position){
     for (std::uint16_t i = 0; i < 10; i++) {
         if (i == player_position) {
             std::cout << "P";
@@ -15,13 +25,33 @@ void print_field(std::uint32_t player_position){
     std::cout << std::endl;
 }
 
+
+bool is_finish(const std::uint32_t player_position) {
+    return player_position == GOAL;
+}
+
+
+void execute_move(std::uint32_t& player_position, const char move){
+    if (move == RIGHT) {
+            std::cout << "Moving to RIGHT" << std::endl;
+            player_position += 1;
+        } else if (move == LEFT && player_position > 0) {
+            std::cout << "Moving to LEFT" << std::endl;
+            if (player_position > 0)
+                player_position -= 1;   
+        } else {
+            std::cout << "'" << move << "' is not a move. Try again..." << std::endl;
+        }
+}
+
+
 void play_game(){
     std::uint32_t player_position = START;
     char user_input;
 
     print_field(player_position);
 
-    while (player_position != GOAL) {
+    while (not is_finish(player_position)) {
         /*
         if (player_position == GOAL) {
             break;
@@ -31,16 +61,7 @@ void play_game(){
         std::cout << "User: ";
         std::cin >> user_input;
 
-        if (user_input == RIGHT) {
-            std::cout << "Moving to RIGHT" << std::endl;
-            player_position += 1;
-        } else if (user_input == LEFT && player_position > 0) {
-            std::cout << "Moving to LEFT" << std::endl;
-            if (player_position > 0)
-                player_position -= 1;   
-        } else {
-            std::cout << "'" << user_input << "' is not a move. Try again..." << std::endl;
-        }
+        execute_move(player_position, user_input);
 
         print_field(player_position);
     }
--- games/computer_game_1/Game.hpp
+++ games/computer_game_1/Game.hpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <cstdint>
 
-constexpr std::uint32_t LEN_X = 10U;
-constexpr std::uint32_t START = 0U;
-constexpr std::uint32_t GOAL = 9U;
-constexpr char LEFT = 'a';
-constexpr char RIGHT = 'd';
+// constexpr std::uint32_t LEN_X = 10U;
+// constexpr std::uint32_t START = 0U;
+// constexpr std::uint32_t GOAL = 9U;
+// constexpr char LEFT = 'a';
+// constexpr char RIGHT = 'd';
 
+void print_field(const std::uint32_t player_position);
+bool is_finish(const std::uint32_t player_position);
+void execute_move(std::uint32_t& player_position, const char move);
 void play_game();
 
